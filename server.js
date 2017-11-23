7012a150fdcacef604d80af8d195e9e4c62f1ad2
mr1971 <martin.s.ransome@gmail.com>
2017-11-22 21:28:14 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var socket = require('socket.io');
 var deck = [];
+var suits = ["spades", "hearts", "clubs", "diams"];
+var ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+var suit;
+var n = 0;
+var s, m;
+var card = {
+    suit: suit,
+    rank: n,
+    icon: suits[s],
+    bgcolor: bgcolor
+}
+var DECK = [];
+var bgcolor = "";
 
 //Set up app
 var app = express();
-var server = app.listen(4000, function(){
-  console.log('Server is listening on port 4000');
+var server = app.listen(4000, function() {
+    console.log('Server is listening on port 4000');
 })
 
 app.use(express.static('public'));
@@ -13,78 +26,54 @@ app.use(express.static('public'));
 //Socket setup
 var io = socket(server);
 
-io.on('connection', function(socket){
-  console.log('Socket connected:  ' + socket.id);
-
-  /*socket.on('FJD', firstJackDeal());*/
-})
+io.on('connection', function(socket) {
+    socket.on('shuffleDeck', function() {
+        DECK = createDeckOfCards();
+        DECK = shuffleDeck(DECK, 5);
+        firstJackDeal();
+    });
+});
 
 function createDeckOfCards() {
-    var suits = ["spades", "hearts", "clubs", "diams"];
-    var ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-
+	 var deck = [];
     for (s in suits) {
         var suit = suits[s][0].toUpperCase();
         var bgcolor = (suit == "S" || suit == "C") ? "black" : "red";
 
         for (n in ranks) {
-          //  table.innerHTML += "<span style='color:" + bgcolor + "'>&" + suits[s] + ";" + ranks[n] + "</span> ";
-            var card = {
+            card = {
                 suit: suit,
                 rank: n,
                 icon: suits[s],
                 bgcolor: bgcolor
             }
-
-            deck.push(card);
-
+				deck.push(card);
         }
-      }
+        var x = n + 1;               
+	}
 }
-
-function shuffleDeck(array,n) {
-    for(c=0;c<n;c++){
-        for (var i = array.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-    return array;
+ 
+    function shuffleDeck(array, n) {
+        var x = Number(n);
+        for (c = 0; c < x; c++) {
+            for (var i = array.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        return array;
+        } 
     }
-}
-
-function firstJackDeal(){
-    console.log('First Jack DEal');
-    var x=0
-    shuffleDeck(deck,1000);
-
-    for(p=0;p<51;p++){
-
-        var card=deck.pop();
-    /*    socket.on(sendCard, function(card){
-        displayCard(card);
-    }); */
-        if(card.rank=='11'){
-            //alert('Player '+x+' is the dealer');
-            break;
-        }
-
-        if(x<5){
-            x=x+1
-            console.log('player : '+x);
-            if (parseInt(x)==4){
-                x=0
+var playerCard = [];
+    function firstJackDeal() {
+        for (crd = 0; crd < 52; crd++) {
+        		playerCard[crd] = DECK.pop();
+            if (playerCard.rank == '11') {            
+                console.log('Card: ' + playerCard.rank);
+                console.log(playerCard);
+                break;
             }
 
         }
-
-        /*socket.on('shuffleDeck', function(data){
-            //shuffleDeck(deck, 7);
-            console.log('shuffling deck');
-        })*/
     }
-
-}
-
-createDeckOfCards();
